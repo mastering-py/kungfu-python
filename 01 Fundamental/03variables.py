3d508b44aa0e2f9f55b9fe3d7d710331b51676ed
Saddam Husein <saddambinhusein2@gmail.com>
2020-06-24 12:52:07 +0800

    Creating Variables

---
+++ 01 Fundamental/03variables.py
@@ -0,0 +1,26 @@
+# Variable is using to store some data
+
+# Assign value to variable
+# Valid
+data = 'This is my data'
+Data = 'This is my data'
+_data = 'This is my data'
+my_data = 'This is my data' # Snake case
+myData = 'This is my data' # Camel case
+
+# Invalid
+"""
+1mydata = 'This is my data'
+my-data = 'This is my data'
+my data = 'This is my data'
+"""
+
+# Output the variables
+print(data)
+
+# Variable names are case sensitive
+aku = 'This is my data 1'
+Aku = 'This is my data 2'
+
+print(aku)
+print(Aku)
