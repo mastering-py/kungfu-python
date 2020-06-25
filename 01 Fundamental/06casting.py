f384ef032e74b870f89efe7cd1914f6b6fc431ae
Saddam Husein <saddambinhusein2@gmail.com>
2020-06-25 17:30:56 +0800

    Convert Data Types

---
+++ 01 Fundamental/06casting.py
@@ -0,0 +1,28 @@
+# Casting is used to convert a data type into another
+
+# a = input('Input number: ') # input data type is string
+#
+# print(a)
+# print(type(a))
+#
+# b = int(input('Input number: ')) # it can change by using int() function
+#
+# print(b)
+# print(type(b))
+
+c = float(23)
+print(c)
+print(type(c))
+
+d = str(23)
+print(d)
+print(type(d))
+
+# e = int('saddam')
+e = int('3')
+print(e)
+print(type(e)) # using alphabet can't convert to integer, but when we change the 'saddam' to numeric it will works
+
+list1 = tuple([1, 2])
+print(list1)
+print(type(list1))
