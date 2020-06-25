bf7483e4e9cc65d9bc6ebfdae2f055f4bc49f572
Saddam Husein <saddambinhusein2@gmail.com>
2020-06-25 12:50:36 +0800

    Data Types in Python

---
+++ 01 Fundamental/04data_types.py
@@ -0,0 +1,49 @@
+print('1. String')
+# a. String literals in python are surrounded by either single quotation marks, double or triple quotation marks.
+print('Single Quotation Mark')
+print("Double Quotation Mark")
+print("""
+
+Triple Quotation Mark
+""") # multiline string
+
+# b. Assign string to a variable and verify the data type using type() function
+a = 'This is string'
+print(a)
+print(type(a))
+
+print('\n2. Numeric')
+# Integer
+b = 123
+print(b)
+print(type(b))
+
+# Float
+c = 1.5
+print(c)
+print(type(c))
+
+# Complex
+d = 2 + 3j
+print(d)
+print(type(d))
+
+print('\n3. Boolean')
+# a. Booleans represent one of two values: True or False.
+e = 2 > 1
+f = 2 < 1
+print(e)
+print(f)
+print(type(e))
+
+print('\n4. List')
+# a. List is a collection which is ordered and changeable. Allows duplicate members. In Python lists are written with square brackets.
+inilist = [1, 2.5, 'tiga', 4]
+
+print(inilist)
+print(type(inilist))
+# 5. Tuple
+
+# 6. Set
+
+# 7. Dictionary
