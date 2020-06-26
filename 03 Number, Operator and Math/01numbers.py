f6fac028ed755ef84f8642a694ec96641d0cfdf2
Saddam Husein <saddambinhusein2@gmail.com>
2020-06-26 17:59:41 +0800

    Numeric Types in Python

---
+++ 03 Number, Operator and Math/01numbers.py
@@ -0,0 +1,17 @@
+# 1. Numeric types in Python
+print('Integer')
+a = 10
+print(a)
+print(type(a))
+
+print('Float')
+b = 10.5
+print(b)
+print(type(b))
+
+print('Complex')
+c = 2+5j
+print(c)
+print(type(c))
+
+print('=' * 100)
