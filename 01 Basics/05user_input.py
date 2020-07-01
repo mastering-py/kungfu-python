87ec76bbf257703d3c22ec8585ffd17dd48aaffc
Saddam Husein <saddambinhusein2@gmail.com>
2020-07-01 16:37:59 +0800

    User input update

--- 01 Basics/05user_input.py
+++ 01 Basics/05user_input.py
@@ -3,3 +3,11 @@
 username = input('Input your username: ')
 
 print(username)
+
+# Math in input
+a = int(input('Input number 1: '))
+b = int(input('Input number 2: '))
+c = a + b
+# or
+# c = int(a + b)
+print(f'{a} + {b} = {c}')
