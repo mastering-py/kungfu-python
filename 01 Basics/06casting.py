5e609303bfaf0b08b15a50825ecb1ed85a863332
Saddam Husein <saddambinhusein2@gmail.com>
2020-07-06 11:38:47 +0800

    Update data types conversion

--- 01 Basics/06casting.py
+++ 01 Basics/06casting.py
@@ -16,19 +16,31 @@ print()
 c = float(23)
 print(c)
 print(type(c))
+print()
 
 #3. Casting using str() function
 d = str(23)
 print(d)
 print(type(d))
+print()
 
 #4. Casting using int() function
 # e = int('saddam')
 e = int('3')
 print(e)
 print(type(e)) # it can't convert to integer using alphabet, but it will works when we change the 'saddam' to the numeric
+print()
 
-#5. Casting using tuple() function
-list1 = tuple([1, 2])
-print(list1)
-print(type(list1))
+#5. Casting using list() function
+tuple1 = (1, 2)
+f = list(tuple1)
+print(f)
+print(type(f))
+print()
+
+#6. Casting using tuple() function
+list1 = [1, 2]
+g = tuple(list1)
+print(g)
+print(type(g))
+print()
