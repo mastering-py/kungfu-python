b2bd1a1c167297084680132cae4485d011bd195a
Saddam Husein <saddambinhusein2@gmail.com>
2020-07-13 12:39:57 +0800

    Update dictionaries built-in functions and methods

--- 07 Dictionaries/02dictionaries_built-in_function_and_methods.py
+++ 07 Dictionaries/02dictionaries_built-in_function_and_methods.py
@@ -62,8 +62,8 @@ print('+' * 100)
 print()
 
 # #2. Methods
-print('Remove using clear(), discard(), remove() and pop methods:')
-# removes all the items from the dictionary by clear()
+print('Remove using clear(), pop() and popitem() methods:')
+print('clear() is using to removes all the items from the dictionary')
 data1 = {
     'anak': 'son',
     'istri': 'wife',
@@ -72,3 +72,110 @@ data1 = {
 }
 data1.clear()
 print(data1)
+
+print('pop() is using to removes the element with the specified key')
+data2 = {
+    'anak': 'son',
+    'istri': 'wife',
+    'ayah': 'father',
+    'ibu': 'mother'
+}
+data2.pop('anak')
+print(data2)
+
+print('popitem() is using to removes the last inserted key-value pair')
+data3 = {
+    'anak': 'son',
+    'istri': 'wife',
+    'ayah': 'father',
+    'ibu': 'mother'
+}
+data3.popitem()
+print(data3)
+print()
+
+print('Returns a copy of the dictionary using copy() methods:')
+data4 = {
+    'anak': 'son',
+    'istri': 'wife',
+    'ayah': 'father',
+    'ibu': 'mother'
+}
+a = data4.copy()
+# a = data4
+a['anak'] = 'lanang'
+print(a)
+print(data4)
+print()
+
+print('Return a list with all dictionary keys with values using items() methods:')
+data5 = {
+    'anak': 'son',
+    'istri': 'wife',
+    'ayah': 'father',
+    'ibu': 'mother'
+}
+b = data5.items()
+print(b)
+print()
+
+print('Return a list with all dictionary keys using keys() methods:')
+data6 = {
+    'anak': 'son',
+    'istri': 'wife',
+    'ayah': 'father',
+    'ibu': 'mother'
+}
+c = data6.keys()
+print(c)
+print()
+
+print('Return a list with all dictionary values using values() methods:')
+data7 = {
+    'anak': 'son',
+    'istri': 'wife',
+    'ayah': 'father',
+    'ibu': 'mother'
+}
+d = data7.values()
+print(d)
+print()
+
+print('Access items using get() methods:')
+data8 = {
+    'anak': 'son',
+    'istri': 'wife',
+    'ayah': 'father',
+    'ibu': 'mother'
+}
+e = data8.get('anak')
+print(e)
+print(data8.get('ibu'))
+print()
+
+print('Returns the value of the specified key using setdefault() methods:')
+data9 = {
+    'anak': 'son',
+    'istri': 'wife',
+    'ayah': 'father',
+    'ibu': 'mother'
+}
+data9.setdefault('kakek', 'grandpa')
+print(data9)
+print()
+
+print('Updates the dictionary with the specified key-value pairs using update() methods:')
+data10 = {
+    'anak': 'son',
+    'istri': 'wife',
+    'ayah': 'father',
+    'ibu': 'mother'
+}
+data11 = {
+    'paman': 'uncle',
+    'bibi': 'auntie'
+}
+data10.update({'kakek': 'grandpa', 'nenek': 'grandma'})
+print(data10)
+data11.update(data10)
+print(data11)
