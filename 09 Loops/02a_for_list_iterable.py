62b844ae1e58d4be5f16eaca61a0a83a7cc722b4
Saddam Husein <saddambinhusein2@gmail.com>
2020-07-20 23:17:18 +0800

    For loop with list as iterable

---
+++ 09 Loops/02a_for_list_iterable.py
@@ -0,0 +1,9 @@
+# A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string)
+
+# List as iterable
+idFoods1 = ['bakso', 'nasi padang', 'soto', 'coto makassar']
+
+for i in idFoods1:
+    print(i)
+
+
