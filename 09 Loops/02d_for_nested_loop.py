9f2151cf2c444bf528f6e843f33b9c82c46a357e
Saddam Husein <saddambinhusein2@gmail.com>
2020-07-21 17:40:51 +0800

    Nesting for

---
+++ 09 Loops/02d_for_nested_loop.py
@@ -0,0 +1,24 @@
+# A nested loop is a loop inside a loop.
+
+# Sample 1
+idFoods = [['bakso', 'nasi padang'], ['soto', 'coto makassar'], ['gule', 'rendang']]
+
+for i in idFoods:
+    print(i)
+    for j in i:
+        print(j)
+
+print('-' * 100)
+
+# Sample 2
+enak1 = ['bakso', 'nasi padang']
+enak2 = ['soto', 'coto makassar']
+enak3 = ['gule', 'rendang']
+
+idFoods2 = [enak1, enak2, enak3]
+
+print(idFoods2)
+
+for k in idFoods2:
+    for m in k:
+        print(m)
