04e7dc46fd86325788a205803ad4449ea64cdc9f
Saddam Husein <saddambinhusein2@gmail.com>
2020-07-22 22:43:29 +0800

    Update nesting for

--- 09 Loops/02e_for_nested_loop.py
+++ 09 Loops/02e_for_nested_loop.py
@@ -1,6 +1,7 @@
 # A nested loop is a loop inside a loop.
 
 # Sample 1
+# Using default for
 idFoods = [['bakso', 'nasi padang'], ['soto', 'coto makassar'], ['gule', 'rendang']]
 
 for i in idFoods: # Layer 1
@@ -22,3 +23,14 @@ print(idFoods2)
 for k in idFoods2:
     for m in k:
         print(m)
+
+print('-' * 100)
+
+# Sample 3
+# Using for in range() function
+idFoods3 = [['bakso', 'nasi padang'], ['soto', 'coto makassar'], ['gule', 'rendang', 'salome', 'cilok']]
+
+for m in range(len(idFoods3)):
+    print(f"{'-' * 5} Row {m + 1} {'-' * 5}")
+    for n in range(len(idFoods3[m])):
+        print(f"Column {n + 1}: {idFoods3[m][n]}")
