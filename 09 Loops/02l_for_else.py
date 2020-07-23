ab9bba7299abca2491fa6d4173eacf664e610988
Saddam Husein <saddambinhusein2@gmail.com>
2020-07-23 23:47:08 +0800

    For loop with else

---
+++ 09 Loops/02l_for_else.py
@@ -0,0 +1,9 @@
+# Without if, we can also combine for loop with else.
+# The else keyword in a for loop specifies a block of code to be executed when the loop is finished
+
+idFoods1 = ['bakso', 'nasi padang', 'soto', 'coto makassar']
+
+for i in idFoods1:
+    print(i)
+else: # this is also part of for
+    print('This is part of for not if')
