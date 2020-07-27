b85b41cbc927267ccc7f97fe66bf4a623549bcb3
Saddam Husein <saddambinhusein2@gmail.com>
2020-07-27 17:31:23 +0800

    Update for in range() function

--- 09 Loops/02d_for_in_range_function.py
+++ 09 Loops/02d_for_in_range_function.py
@@ -1,4 +1,4 @@
-# To loop through a set of code a specified number of times, we can use the range() function
+# To loop through a set of code a specified number of times (e.g. Iterating by Sequence Index), we can use the range() function
 
 # Sample 1
 # Without iterable
@@ -9,7 +9,7 @@ for i in range(5):  # From 0 to 4 (5 not including)
 print('-' * 100)
 
 # Type 2
-for j in range(5, 10):  # Start from 5 to 9 (10 not including)
+for j in range(5, 10):  # Start paramater is from 5 to 9 (10 not including)
     print(j)
 
 print('-' * 100)
@@ -36,5 +36,14 @@ lenIdf1 = len(idFoods1)  # using len() function to determine how many items a li
 
 for m in range(lenIdf1):
     print(f'{m + 1}. {idFoods1[m]}')
+
+print('-' * 100)
+
+# Type 3 Looping to some of index list
+idFoods2 = ['bakso', 'nasi padang', 'soto', 'coto makassar']
+lenIdf2 = len(idFoods2[3])
+
+for n in range(lenIdf2):
+    print(idFoods2[3][n])
 
 print('-' * 100)
