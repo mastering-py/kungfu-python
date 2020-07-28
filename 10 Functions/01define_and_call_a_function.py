50be2214fa8828404321dac25c809254abd69cb9
Saddam Husein <saddambinhusein2@gmail.com>
2020-07-28 18:22:40 +0800

    Creating and calling function

---
+++ 10 Functions/01define_and_call_a_function.py
@@ -0,0 +1,18 @@
+"""
+A function is a block of code which only runs when it is called. You can pass data, known as parameters,
+into a function. A function can return data as a result. Functions provide better modularity for your application and a
+high degree of code reusing. As you already know, Python gives you many built-in functions like print(), etc. but
+you can also create your own functions. These functions are called user-defined functions.
+"""
+
+
+def main():
+    print('Hello, this is create by function')
+
+
+main()
+""" 
+notes:
+- to create a function, use the def keyword e.g. def main()
+- to call a function, use the function name followed by parenthesis e.g. main()
+"""
