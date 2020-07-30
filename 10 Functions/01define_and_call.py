e6e31408e4ebadb54ea923ae75a8394a59eee445
Saddam Husein <saddambinhusein2@gmail.com>
2020-07-30 14:06:29 +0800

    Update creating and calling function

--- 10 Functions/01define_and_call.py
+++ 10 Functions/01define_and_call.py
@@ -6,13 +6,16 @@ you can also create your own functions. These functions are called user-defined 
 """
 
 
-def main():
-    print('Hello, this is create by function')
+def main():  # define
+    print('Hello, this is create by function')  # relies on indentation too
 
 
+main()  # call
+main()
 main()
 """ 
 notes:
-- to create a function, use the def keyword e.g. def main()
+- to create or define a function, use the def keyword followed by the function name and parentheses e.g. def main()
 - to call a function, use the function name followed by parenthesis e.g. main()
+- rules for function names is same as variable names
 """
