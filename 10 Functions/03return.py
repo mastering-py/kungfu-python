679cbb5381c2433f58dae001ad6698f5f5c7e4fc
Saddam Husein <saddambinhusein2@gmail.com>
2020-07-30 14:08:25 +0800

    Return values

---
+++ 10 Functions/03return.py
@@ -0,0 +1,26 @@
+
+# Sample 1
+def main():
+    # name = 'Hello saddam'
+    # return name
+    return 'Hello saddam'
+
+
+# print(main())
+a = main()  # assign to a variable
+print(a)
+
+print('-' * 100)
+
+# Sample 2
+def items():
+    list1 = ['satu', 'dua', 'tiga', 'empat']
+    return list1
+
+
+def main():
+    for i in items():
+        print(i)
+
+
+main()
