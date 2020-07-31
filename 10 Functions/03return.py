55b05738f28770f5d4e3ea62d353057398035cf8
Saddam Husein <saddambinhusein2@gmail.com>
2020-07-31 22:20:53 +0800

    Update return values

--- 10 Functions/03return.py
+++ 10 Functions/03return.py
@@ -1,9 +1,10 @@
+# To let a function return a value, use the return statement
 
 # Sample 1
 def main():
-    # name = 'Hello saddam'
-    # return name
-    return 'Hello saddam'
+    # return 'Hello saddam'
+    name = 'Hello saddam'
+    return name
 
 
 # print(main())
@@ -13,6 +14,23 @@ print(a)
 print('-' * 100)
 
 # Sample 2
+def val1():
+    value1 = int(input('Input value 1: '))
+    return value1
+
+
+def val2():
+    value2 = int(input('Input value 2: '))
+    return value2
+
+
+a = val1()
+b = val2()
+print(f'{a} + {b} = {a + b}')
+
+print('-' * 100)
+
+# Sample 3
 def items():
     list1 = ['satu', 'dua', 'tiga', 'empat']
     return list1
