26f4d754c9a3e64631863b0ef68333e28f13feb8
Saddam Husein <saddambinhusein2@gmail.com>
2020-08-18 21:27:47 +0800

    Default arguments

---
+++ 10 Functions/03c_default_arguments.py
@@ -0,0 +1,29 @@
+"""
+Default Arguments / Default Parameter Value Arguments:
+Allow us to provide defaults for corresponding formal arguments in the function definition. Python
+uses these defaults if corresponding actual arguments are not passed in the function call.
+"""
+
+# Sample 1 (One argument)
+def args(a = 10):
+    print(f'This is number {a}')
+
+
+args()  # If we call the function without argument, it uses the default parameter values
+args(2)
+
+print('-' * 100)
+
+# Sample 2 (Many arguments)
+def args(a, b = 'Morning'):
+    print(f'Hello Mr. {a}. Good {b}!')
+
+
+args('Rahmat')  # a = 'Rahmat'. If the b argument is not set, the default parameter given in the definition will be used
+args('Saddam', 'Evening')
+args(b = 'Afternoon', a = 'fuad')  # the arguments set by keywords
+
+# Sample 3 (Invalid default parameter value)
+# def args(a = 'saddam', b):
+# def args(a = 'saddam', b, c = 'rahmat'):
+# def args(a, b = 'saddam', c):
