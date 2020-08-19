51b1455d2ff9dd139d1490ecb52cf2dcbdef93ab
Saddam Husein <saddambinhusein2@gmail.com>
2020-08-19 10:07:05 +0800

    Non keywords arguments (Tuples)

---
+++ 10 Functions/03d_variable_length_non_keywords_arguments(tuples).py
@@ -0,0 +1,34 @@
+"""
+Variable Length Non keyword arguments (Tuples):
+Variable length non keyword arguments are useful when we are not certain about number of arguments that are required in
+the function definition or function would receive. Any left over actual (positional) arguments are captured by python
+as a tuple, which is available through variable which is having prefixed * in the function definition.
+"""
+
+# Sample 1: Single non keyword argument
+def args(*a):
+    print('Non Keyword Arguments:', a)
+    print('-' * 10)
+    for i in a:
+        print('Non Keyword Argument:', i)
+
+
+args(1, 2, 3, 4, 'lima', 6, 7)  # the arguments packaged to tuple
+
+print('-' * 100)
+
+# Sample 2: Many non keyword argument
+def args(a, b, *c):
+    print('Positional Argument:', a)
+    print('Positional Argument:', b)
+    print('-' * 10)
+    for i in c:
+        print('Non Keyword Argument:', i)
+
+
+args(10, 2)
+args(1, 2, 3, 4, 'lima', 6, 7)  # (*c = 3, 4, 'lima', 6, 7). captured by python as a tuple
+
+# Sample 3: Invalid non keyword arguments
+# def args(a, *b, *c)  # two non keywords arguments in parameter
+# def args(*a, b, c)  # first slots given for positional arguments
