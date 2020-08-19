2f7ec52541ea688988b9ef246ded53778d725042
Saddam Husein <saddambinhusein2@gmail.com>
2020-08-19 12:53:36 +0800

    Keyword only arguments

--- 10 Functions/03b_keyword_only_arguments.py
+++ 10 Functions/03b_keyword_only_arguments.py
@@ -9,9 +9,9 @@ def args(a, b):
 
 args(a = 'Rahmat', b = 23)
 args(b = 31, a = 'saddam')  # Since we are passing arguments by their names/keywords order or position doesn’t matter
-args('fuad', b = 20)  # We can mix both positional and keyword arguments but keyword arguments should follow positional arguments
+args('fuad', b = 20)  # We can combine both positional and keyword arguments. but positional arguments should follows by keyword arguments
 
 # invalid keyword only arguments
 # args(50, 'mike')  # the position is incorrect
 # args('saddam')  # the count between arguments and parameters don’t match
-# args(b = 18, 'fitri')  # If we break the rule and pass arguments in such a way that positional arguments follow keyword arguments which is a syntax error.
+# args(b = 18, 'fitri')  # If we break the rule and pass arguments in such a way that keyword arguments follows by positional arguments which is a syntax error.
