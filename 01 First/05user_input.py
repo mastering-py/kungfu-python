2098cd7f4d72b14255415a2cda8a0de0ad0a76f8
Saddam Husein <saddambinhusein2@gmail.com>
2020-08-19 10:05:45 +0800

    User input

--- 01 First/05user_input.py
+++ 01 First/05user_input.py
@@ -5,6 +5,8 @@ username = input('Input your username: ')
 print(username)
 print(type(username)) # the input data type is string
 
+print('-' * 100)
+
 # Math in input
 a = int(input('Input number 1: '))
 b = int(input('Input number 2: '))
@@ -12,3 +14,14 @@ c = a + b
 # or
 # c = int(a + b)
 print(f'{a} + {b} = {c}')
+
+print('-' * 100)
+
+# Other sample
+print('Type your name:')
+name = input()
+print('Type your password:')
+pwd = int(input())
+print('=' * 10)
+print('Your name is', name)
+print('Your password is', pwd)
