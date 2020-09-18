245af414898a2baefb4dccd3547aa5d9d63386d8
Saddam Husein <saddambinhusein2@gmail.com>
2020-09-18 23:54:32 +0800

    Object in Python

---
+++ 13 OOP/02object.py
@@ -0,0 +1,17 @@
+# Sample 1
+class Heroes:
+    # default attributes
+    name = 'name'
+    power = 'max'
+    health = 'max'
+
+
+# Create an object named superman
+superman = Heroes()
+superman.name = 'Superman'
+superman.power = 100
+superman.health = 100
+
+print(superman.name)
+print(superman.power)
+print(superman.health)
