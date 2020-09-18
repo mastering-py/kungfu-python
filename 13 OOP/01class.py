394de5725d33518254deb567414906540b280fc3
Saddam Husein <saddambinhusein2@gmail.com>
2020-09-18 23:45:18 +0800

    Class in Python

---
+++ 13 OOP/01class.py
@@ -0,0 +1,12 @@
+# Almost everything in Python is an object, with its properties and methods.
+# To create a class, use the keyword class.
+
+class Heroes:
+    name = 'Superman'
+    power = 100
+    health = 100
+
+
+print(Heroes.name)
+print(Heroes.power)
+print(Heroes.health)
