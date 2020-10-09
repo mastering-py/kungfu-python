00e7b45b4e1350ee8f1b1c1854aef4bc54b83386
Saddam Husein <saddambinhusein2@gmail.com>
2020-10-09 21:27:05 +0800

    Update class in Python

--- 13 OOP/01class.py
+++ 13 OOP/01class.py
@@ -1,15 +1,42 @@
-# Almost everything in Python is an object, with its properties and methods.
+# Almost everything in Python is an object, with its attributes and methods.
 # To create a class, use the keyword class.
 
-# create a class
-class Heroes:
-    # attributes
+# Sample 1: Create a class without any attributes
+class Heroes1:
+    pass
+
+
+print(Heroes1())  # The output is memory address of Heroes class
+
+print('-' * 100)
+
+# Sample 2: Create a class with its attributes
+# Type 1: Attributes outside of the class
+class Heroes2:
+    pass
+
+
+# Attributes:
+Heroes2.name = 'Superman'
+Heroes2.power = 100
+Heroes2.health = 100
+
+# Access the class variables
+print(Heroes2.name)
+print(Heroes2.power)
+print(Heroes2.health)
+
+print()
+
+# Type 2: Attributes inside of the class
+class Heroes3:
+    # Attributes for Heroes3 class (Class Variables)
     name = 'Superman'
     power = 100
     health = 100
 
 
-# display class attributes
-print(Heroes.name)
-print(Heroes.power)
-print(Heroes.health)
+# Access the class variables
+print(Heroes3.name)
+print(Heroes3.power)
+print(Heroes3.health)
