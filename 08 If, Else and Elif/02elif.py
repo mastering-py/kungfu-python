d5ea5023a4696d220723930e1ffaf311593b43dd
Saddam Husein <saddambinhusein2@gmail.com>
2021-04-21 13:19:48 +0800

    Adding sample 2 into elif

--- 08 If, Else and Elif/02elif.py
+++ 08 If, Else and Elif/02elif.py
@@ -1,5 +1,6 @@
 # Elif statement that is used in Python when we need to choose between more than two alternatives
 
+# Sample 1
 score = int(input('Input your passing grade (0 - 100): '))
 
 if 80 <= score <= 100:
@@ -14,3 +15,22 @@ elif score < 0 or score > 100:
     print('Enter the correct value')
 else:
     print('Your passing grade is Unsatisfactory')
+
+print('-' * 100)
+
+# Sample 2
+score2 = int(input('Input your other passing grade (0 - 100): '))
+result = None
+
+if score2 >= 80:
+    result = 'You got A'
+elif score2 >= 70:
+    result = 'You got B'
+elif score2 >= 60:
+    result = 'You got C'
+elif score2 >= 50:
+    result = 'You got D, reexam'
+else:
+    result = 'Your passing grade is Unsatisfactory'
+
+print(result)
